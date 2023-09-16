005dea486c5348e020c94ef98cb0ca3e85b58f6f
Bibou1494 <132447113+Bibou1494@users.noreply.github.com>
2023-09-16 19:48:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+const Banchojs = require("bancho.js");
 
+const USERNAME = "Nxd_Bibou1494"
+const PASSWORD = "11916c5d"
+
+const client = new Banchojs.BanchoClient({
+  username: USERNAME,
+  password: PASSWORD
+});
+
+const prefix = "!";
+
+const startOsuBot = async () => {
+  try {
+    await client.connect();
+    console.log("osu!Bancho Connected...");
+
+    client.on("PM", async ({ message, user }) => {
+      //Check if message was sent by ourselves
+      if (user.ircUsername === USERNAME) return;
+
+      //Check for message prefix
+      if (message[0] !== "!") return;
+
+      const command = message.split(" ")[0].toLowerCase();
+
+      switch (command) {
+        case prefix + "hello":
+          return await user.sendMessage(`Hello there ${user.ircUsername}`);
+		case prefix + "help":
+			return await user.sendMessage(`All command are in [https://github.com/Bibou1494/BibouOsu-Bancho-Bot GitHub]`)
+      };
+    });
+
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+startOsuBot();
